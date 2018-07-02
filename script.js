73101f6d0450d21f03f624d85db18b53eada5a58
msafadieh <me@msafadieh.com>
2018-07-03 01:55:54 +0300

    fixed money display

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var currentSize = 50;
 var click = 0;
 var money = 0
 var random = Math.floor(Math.random() * 128 + 1);
+function displayMoney(value) {
+	if (value % 100 == 0) { return value/100 + '.00'; }
+	else if (value % 10 == 0) { return value/100 + '0' }
+	else { return value/100 }
+}
 function onClick() {
 	if (click == random) {
 		document.getElementById('balloon').style.display = 'none';
@@ -15,11 +20,11 @@ function onClick() {
 		click += 1;
 		document.getElementById('balloon').style.width = currentSize + 'px';
 		money += 5;
-		document.getElementById('money').innerHTML = '$' + money/100;
+		document.getElementById('money').innerHTML = '$' + displayMoney(money);
 	}
 }
 function cashOut() {
 	document.getElementById('cashOut').style.display = 'none';
 	document.getElementById('more').style.display = 'none';
-	document.getElementById('money').innerHTML = "You cashed out: $" + money/100;
+	document.getElementById('money').innerHTML = "You cashed out: $" + displayMoney(money);
 }
