c120ec86f455bc921fc9d798724c7a25a1af96d7
msafadieh <me@msafadieh.com>
2018-07-02 03:16:09 +0300

    fixed probability and value increase

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 var currentSize = 50;
 var click = 0;
 var money = 0
-var random = Math.floor(Math.random() * 5 + 5);
+var random = Math.floor(Math.random() * 128 + 1);
 function onClick() {
   if (click == random) {
     document.getElementById('balloon').style.display = 'none';
     document.getElementById('popped').style.display = 'block';
     document.getElementById('money').innerHTML = 'You lost all your money!';
-    click += 1;
   }
-  else if (click > random) {
-    currentSize = 50;
-    click = 0;
-    random = Math.floor(Math.random() * 5 + 5);
-    money = 0;
-    document.getElementById('balloon').style.width = currentSize + 'px';
-    document.getElementById('money').innerHTML = '$' + money;
-    document.getElementById('balloon').style.display = 'block';
-    document.getElementById('popped').style.display = 'none';
-  }
-  else {
+  else if (click < random) {
     currentSize += 25;
     click += 1;
     document.getElementById('balloon').style.width = currentSize + 'px';
     money += 5;
-    document.getElementById('money').innerHTML = '$' + money;
+    document.getElementById('money').innerHTML = '$' + money/100;
   }
 }
