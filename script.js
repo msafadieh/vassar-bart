c332df42c5a99dce3231d734c08955ecfab39f2b
msafadieh <me@msafadieh.com>
2018-07-02 03:41:39 +0300

    changed balloon file, added cash out

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-var currentSize = 150;
+var currentSize = 50;
 var click = 0;
 var money = 0
 var random = Math.floor(Math.random() * 128 + 1);
 function onClick() {
-  if (click == random) {
-    document.getElementById('balloon').style.display = 'none';
-    document.getElementById('popped').style.display = 'block';
-    document.getElementById('money').innerHTML = 'You lost all your money!';
-  }
-  else if (click < random) {
-    currentSize += 1; 
-    click += 1;
-    document.getElementById('balloon').style.width = currentSize + 'px';
-    money += 5;
-    document.getElementById('money').innerHTML = '$' + money/100;
-  }
+	if (click == random) {
+		document.getElementById('balloon').style.display = 'none';
+		document.getElementById('popped').style.display = 'block';
+		document.getElementById('money').innerHTML = 'You lost all your money!';
+		document.getElementById('cashOut').style.display = 'none';
+		document.getElementById('more').style.display = 'none';
+	}
+	else if (click < random) {
+		currentSize += 1; 
+		click += 1;
+		document.getElementById('balloon').style.width = currentSize + 'px';
+		money += 5;
+		document.getElementById('money').innerHTML = '$' + money/100;
+	}
+}
+function cashOut() {
+	document.getElementById('cashOut').style.display = 'none';
+	document.getElementById('more').style.display = 'none';
+	document.getElementById('money').innerHTML = "You cashed out: $" + money/100;
 }
